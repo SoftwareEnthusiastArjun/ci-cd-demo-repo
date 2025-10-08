dcefdbb37aa76116090a27986088559cc243bb25
Arjun <achuarjun11111@gmail.com>
2025-10-08 15:43:17 +0200

    imported lib for test in test

--- test_app.py
+++ test_app.py
@@ -1,7 +1,9 @@
 from app import add
+import pytest
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
     assert add(-1, -1) == -2
+    pytest.raises(TypeError, add, 'a', 1) 
